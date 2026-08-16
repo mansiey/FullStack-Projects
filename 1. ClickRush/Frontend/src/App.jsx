197408c38fed27e0c27e7fa854109812f12e1c39
mansiey <mansi46rd@gmail.com>
2026-08-16 14:10:07 +0530

    Add Protected Routes

--- 1. ClickRush/Frontend/src/App.jsx
+++ 1. ClickRush/Frontend/src/App.jsx
@@ -1,15 +1,20 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import ProtectedRoute from './components/ProtectedRoute';
 import Login from './pages/login';
 import Signup from './pages/signup';
 
-function App(){
-  return(
+function App() {
+  return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<h1>ClickRush</h1>} />
-        <Route path='/signup' element={ <Signup /> } />
-        <Route path='/login' element={ <Login />} />
-        <Route path='/games' element={<h1>Games</h1>} />
+        <Route path='/signup' element={<Signup />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/games' element={
+          <ProtectedRoute>
+            <h1>Games</h1> 
+          </ProtectedRoute>
+        } />
         <Route path='/profile' element={<h1>Profile</h1>} />
         <Route path='/leaderboard' element={<h1>Leaderboard</h1>} />
 
